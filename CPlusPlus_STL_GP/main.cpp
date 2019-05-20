86dfac8cef15a825be045d4ff7c4f2339019d3d1
Junjie Zhou <randybrown18@me.com>
2019-05-20 20:48:02 +0800

    Initial Commit

---
+++ CPlusPlus_STL_GP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPlusPlus_STL_GP
+//
+//  Created by 周俊杰 on 2019/5/20.
+//  Copyright © 2019 Junjie Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
